eaf02bf0ef3423abae3cd863b9aeaf6977c342c5
Olli Huurinainen <huurinainen.olli@gmail.com>
2024-08-13 20:09:57 +0300

    env: Add option to customize server address

--- vm/development/files/zyn-run-web-client.sh
+++ vm/development/files/zyn-run-web-client.sh
@@ -2,12 +2,17 @@
 
 set -euo pipefail
 
+server_address=${ZYN_SERVER_ADDRESS:-ws://localhost:8080}
+
+echo "Starting webclient using server address \"$server_address\""
+echo "Customize server address with environment variable ZYN_SERVER_ADDRESS"
+
 zyn-webserver \
     8081 \
     10.0.2.15 \
     8080 \
     --no-tls \
-    --server-websocket-address ws://localhost:8080 \
+    --server-websocket-address "$server_address" \
     --debug-protocol \
     --debug-tornado \
     -vv \
