9c4eed10aacb73502ecfd27dfbf5f34e1b177386
Olli Huurinainen <huurinainen.olli@gmail.com>
2024-08-13 19:50:17 +0300

    client,web: Cleanup and remove references to old implementation

--- py/setup.py
+++ py/setup.py
@@ -21,28 +21,7 @@ setup(
         ]
     },
     data_files=[
-        ('zyn-web-static/3pp/icons8', [
-            "zyn/client/zyn-web-static/3pp/icons8/cancel.png"
-        ]),
-        ('zyn-web-static/3pp/jsdiff', [
-            "zyn/client/zyn-web-static/3pp/jsdiff/diff.min.js",
-            "zyn/client/zyn-web-static/3pp/jsdiff/LICENSE",
-        ]),
-        ('zyn-web-static/3pp/pdfjs', [
-            "zyn/client/zyn-web-static/3pp/pdfjs/LICENSE",
-            "zyn/client/zyn-web-static/3pp/pdfjs/pdf.js",
-            "zyn/client/zyn-web-static/3pp/pdfjs/pdf.worker.js",
-        ]),
-        ('zyn-web-static/3pp/showdownjs', [
-                "zyn/client/zyn-web-static/3pp/showdownjs/LICENSE",
-                "zyn/client/zyn-web-static/3pp/showdownjs/showdown.min.js",
-        ]),
-        ('zyn-web-static/3pp/w3css', [
-                "zyn/client/zyn-web-static/3pp/w3css/w3.css",
-        ]),
         ('zyn-web-static', [
-            "zyn/client/zyn-web-static/zyn-client.js",
-            "zyn/client/zyn-web-static/zyn-connection.js",
             "zyn/client/zyn-web-static/zyn.css",
             "zyn/client/zyn-web-static/zyn.js",
         ]),
