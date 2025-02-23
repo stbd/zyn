de3e5393a460d6908ccf7d706e4ff35480b76f02
Olli Huurinainen <huurinainen.olli@gmail.com>
2025-02-23 20:26:55 +0200

    client,web: Fix PDF rendering

--- js/src/file.mjs
+++ js/src/file.mjs
@@ -403,7 +403,6 @@ class PdfFile extends Base {
 
     if (open_rsp.size === 0) {
       this._content = '';
-      console.log('empty')
       this.render()
     } else {
       this.read_file_content(
@@ -411,8 +410,7 @@ class PdfFile extends Base {
         open_rsp.size,
         (data, revision) => {
           this._revision = revision
-          this._content = decode_from_bytes(data);
-          console.log(`content ${this._content.length}` )
+          this._content = data;
           this.render();
         }
       );
@@ -421,30 +419,49 @@ class PdfFile extends Base {
 
   render() {
     console.log(`Rendering PDF`)
-    let canvas = this._client.ui().create_file_content_canvas();
-    let context = canvas.getContext('2d');
     var scale = 1.5;
+    let ui = this._client.ui();
 
     GlobalWorkerOptions.workerSrc = `${this._client.ui().get_browser_url().origin}/static/pdf.worker.mjs`
 
     getDocument({data: this._content}).promise.then(function(pdf) {
-      console.log('PDF loaded');
 
-      let pageNumber = 1;
-      pdf.getPage(pageNumber).then(function(page) {
+      console.log('PDF loaded');
+      let pages = [];
+      let page_number = 1;
+      const number_of_pages = pdf.numPages;
+
+      const render_page = (page) => {
+
+        let canvas = ui.create_canvas();
+        let context = canvas.getContext('2d');
         var viewport = page.getViewport({scale: scale});
         var render_context = {
           canvasContext: context,
           viewport: viewport
         };
-        page.render(render_context).promise.then(function () {
-          console.log('Page rendered');
-        });
-      });
+        page.render(render_context);
+        pages.push(canvas);
+
+        page_number += 1;
+        if (page_number <= number_of_pages) {
+          pdf.getPage(page_number).then(render_page);
+        } else {
+          let content = ui.get_file_content();
+          content.innerHTML = '';
+          for (const c of pages) {
+            content.appendChild(c);
+          }
+          ui.hide_modals();
+        }
+      };
+
+      pdf.getPage(page_number).then(render_page);
+
     }, function (reason) {
       console.error(reason);
+      ui.hide_modals();
     });
-    this._client.ui().hide_modals();
   }
 }
 
