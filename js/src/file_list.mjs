f16acfc0614c2fea2bd15e762b159e1ccd96dcc3
Olli Huurinainen <huurinainen.olli@gmail.com>
2025-12-13 17:00:56 +0200

    client,web: Add support for tags in list files

--- js/src/file_list.mjs
+++ js/src/file_list.mjs
@@ -21,6 +21,7 @@ const ELEMENT_ID = "zyn-list-element";
 const ELEMENT_TEXT_ID = "zyn-list-element-text";
 const ELEMENT_BUTTON_SAVE_ID = "zyn-list-element-save";
 const ELEMENT_BUTTON_DELETE_ID = "zyn-list-element-delete";
+const ELEMENT_TAG_SELECTOR = 'zyn-list-element-tag-selector'
 
 class ListElement {
   constructor(id, data) {
@@ -55,6 +56,9 @@ class ListElement {
   add_tag(tag) {
     this._tags.push(tag);
     this._tags = [...new Set(this._tags)]; 
+  }
+  remove_tag(tag) {
+    this._tags = this._tags.filter(n => n !== tag);
   }
   data_size() { 
     if (!this.is_initialized()) {
@@ -267,6 +271,8 @@ class List {
           element.clear_move();
         }
       )
+    } else {
+      log('Element not changed');
     }
     return element;
   }
@@ -294,7 +300,8 @@ class ListFile extends Base {
   static is_editable = false;
   static default_open_mode = OpenMode.edit;
   static ELEMENT_NAME_LIST = 'zyn-list-page-elements';
-  static ELEMENT_NAME_ADD_BUTTON = 'zyn-list-page-add-button';
+  static ELEMENT_NAME_ADD_ROW_BUTTON = 'zyn-list-page-add-tag-button';
+  static ELEMENT_NAME_TAGS_CONTAINER = 'zyn-list-page-tags-area';
 
   constructor(open_rsp, client, filename, mode) {
     super(open_rsp, client, filename);
@@ -306,6 +313,8 @@ class ListFile extends Base {
       'simplifiedAutoLink': true,
       'simpleLineBreaks': true,
     });
+    this._tags = [];
+    this._active_tags = [];
 
     this._client.ui().show_loading_modal('Loading file content...');
     this._client.ui().file_area_button_done(false);
@@ -388,7 +397,8 @@ class ListFile extends Base {
 
     this._client.ui().set_file_content(
       `
-<button id="${ListFile.ELEMENT_NAME_ADD_BUTTON}" class="zyn-button m-3 px-5 h-[3rem]" >Add new row</button>
+<button id="${ListFile.ELEMENT_NAME_ADD_ROW_BUTTON}" class="zyn-button m-3 px-5 h-[3rem] inline" >Add new row</button>
+<div id="${ListFile.ELEMENT_NAME_TAGS_CONTAINER}" class="inline-flex"></div>
 <div id="${ListFile.ELEMENT_NAME_LIST}" class="flex flex-col m-4">
 </div>
       `,
@@ -397,18 +407,100 @@ class ListFile extends Base {
     this._client.ui().hide_modals();
   }
 
-  render_callback() {
-    for (const [_, element] of this._list_content.elements()) {
-      this._add_list_element(element);
-    }
+  update_tags_container() {
+    let container = this._client.ui().document().getElementById(ListFile.ELEMENT_NAME_TAGS_CONTAINER);
+    container.textContent = '';
+   
+    const new_tag_input = this._client.ui().document().createElement('input');
+    new_tag_input.classList.add('border-2', 'rounded-sm')
+    new_tag_input.text = 'Tag name';
+    container.appendChild(new_tag_input);
 
+    const add_tag_button = this._client.ui().document().createElement('button');
+    add_tag_button.classList.add('border', 'rounded-lg', 'hover:bg-zinc-100', 'mx-2', 'px-1');
+    add_tag_button.innerText = 'Add Tag'
+    add_tag_button.type = 'text'
+    add_tag_button.size = 10;
+    add_tag_button.addEventListener('click', () => {
+      const tag = new_tag_input.value.trim();
+      if (tag.length == 0) {
+        return ;
+      }
+      log(`Adding tag "${tag}" to file list`);
+      this._tags.push(tag);
+      this.update_tags_container();
+    });
+    container.appendChild(add_tag_button);
+
+    for (const tag of this._tags) {
+      const tag_text = this._client.ui().document().createElement('span');
+      tag_text.id = ELEMENT_TAG_SELECTOR;
+      tag_text.classList.add('underline', 'cursor-pointer', 'px-1', 'rounded-md')
+      tag_text.dataset.enabled = 'false'
+      tag_text.innerText = tag;
+      container.appendChild(tag_text);
+      tag_text.addEventListener('click', () => {
+
+        const is_enabled = tag_text.dataset.enabled !== 'false';
+        if (is_enabled) {
+          tag_text.dataset.enabled = 'false';
+          tag_text.classList.remove('bg-zyn_green');
+        } else {
+          tag_text.dataset.enabled = 'true';
+          tag_text.classList.add('bg-zyn_green');
+        } 
+        
+        let active_tags = [];
+        for (const tag of this._client.ui().document().querySelectorAll(`#${ELEMENT_TAG_SELECTOR}`)) {
+          if (tag.dataset.enabled === 'true') {
+            active_tags.push(tag.textContent);
+          }
+        }
+        
+        log(`Filtering with tags "${active_tags}"`);
+        
+        const list = this._client.ui().document().getElementById(ListFile.ELEMENT_NAME_LIST);
+        for (const row of list.querySelectorAll(`#${ELEMENT_ID}`)) {
+          const e = this._list_content.find_element(parseInt(row.dataset.row_id));
+          let is_filtered = true;
+
+          if (active_tags.length === 0) {
+            is_filtered = false;
+          } else {
+            is_filtered = false;
+            for (const active_tag of active_tags) {
+              if (!e.tags().includes(active_tag)) {
+                is_filtered = true;
+                break ;
+              }
+            }
+          }
+
+          if (is_filtered) {
+            row.classList.add('hidden')
+          } else {
+            row.classList.remove('hidden')
+          }
+        }
+      });
+    }
+  }
+
+  render_callback() {
+    this._tags = [];
     this._sortable = new Sortable(
       this._client.ui().document().getElementById(ListFile.ELEMENT_NAME_LIST),
       {
         onEnd: (e) => this.handle_element_moved(e),
       },
     );
-    this._client.ui().document().getElementById(ListFile.ELEMENT_NAME_ADD_BUTTON).addEventListener(
+
+    for (const [_, element] of this._list_content.elements()) {
+      this._add_list_element(element);
+    }
+    this.update_tags_container();
+
+    this._client.ui().document().getElementById(ListFile.ELEMENT_NAME_ADD_ROW_BUTTON).addEventListener(
       'click',
       (_) => this._handle_add_button_clicked(),
     );
@@ -417,7 +509,6 @@ class ListFile extends Base {
   handle_element_moved(event) {
     this._set_row_editable(event.item, true);
     this._list_content.find_element(parseInt(event.item.dataset.row_id)).move_to_index(event.newIndex);
-    this._disable_sorting();
   }
 
   _set_row_editable(row, editable) {
@@ -428,10 +519,12 @@ class ListFile extends Base {
       save_button.classList.remove('hidden');
       text.setAttribute('contenteditable', true);
       text.classList.add('bg-zyn_green', 'rounded-md');
+      this._disable_sorting();
     } else {
       text.classList.remove('bg-zyn_green');
       text.setAttribute('contenteditable', false);
       save_button.classList.add('hidden');
+      this._enable_sorting();
     }
   }
 
@@ -440,25 +533,91 @@ class ListFile extends Base {
     const text = this._client.ui().document().createElement('span');
     const save_button = this._client.ui().document().createElement('button');
     const delete_button = this._client.ui().document().createElement('button');
+    const tags_container = this._client.ui().document().createElement('div');
 
-    row.classList.add('flex', 'rounded-md', 'border-2', 'my-1');
+    const add_tag_to_row = (tags_container, tags) => {
+      for (const tag of tags) {
+        const text_tag = this._client.ui().document().createElement('span');
+        text_tag.innerText = tag;
+        text_tag.classList.add('px-1', 'hover:underline', 'cursor-pointer')
+        tags_container.insertBefore(text_tag, tags_container.firstChild);
+
+        text_tag.addEventListener('click', () => {
+          log(`Removing tag "${tag}" from element ${element.id()}`);
+          element.remove_tag(tag);
+          tags_container.removeChild(text_tag);
+          this._set_row_editable(row, true);
+        });
+      }
+    }
+    this._tags = this._tags.concat(element.tags());
+    this._tags = [...new Set(this._tags)];
+
+    row.classList.add('flex', 'rounded-md', 'border-2', 'my-2', 'relative');
     row.id = ELEMENT_ID
-    text.classList.add('flex-1', 'mx-2', 'my-2', 'p-1');
+    text.classList.add('flex-1', 'mx-2', 'my-3', 'p-1');
     text.id = ELEMENT_TEXT_ID
-    save_button.classList.add('flex', 'justify-end', 'zyn-button', 'mx-2', 'px-2', 'my-2');
+    save_button.classList.add('flex', 'justify-end', 'zyn-button', 'mx-2', 'px-2', 'my-3');
     save_button.id = ELEMENT_BUTTON_SAVE_ID;
-    delete_button.classList.add('flex', 'justify-end', 'zyn-button', 'mx-2', 'px-2', 'my-2');
+    delete_button.classList.add('flex', 'justify-end', 'zyn-button', 'mx-2', 'px-2', 'my-3');
     delete_button.id = ELEMENT_BUTTON_DELETE_ID;
+    tags_container.classList.add('absolute', '-top-4', 'left-3', 'z-9', 'bg-white', 'px-2', 'inline-block')
+
+    const add_tag_button = this._client.ui().document().createElement('button');
+    add_tag_button.classList.add('border', 'rounded-lg', 'hover:bg-zinc-100', 'px-3')
+    add_tag_button.innerText = 'Add Tag'
+    add_tag_button.addEventListener('click', () => {
+
+      const add_tag_popup = this._client.ui().document().createElement('div');
+      add_tag_popup.classList.add(
+        'absolute', 'border', 'rounded-lg', 'p-2', 'left-full', '-top-3', 'min-w-40', 
+        'flex', 'flex-col', 'z-9', 'bg-white'
+      );
+
+      const close_tag_popup_button = this._client.ui().document().createElement('button');
+      close_tag_popup_button.classList.add('zyn-button', 'flex-1', 'my-2')
+      close_tag_popup_button.innerText = 'Close';
+      close_tag_popup_button.addEventListener('click', () => {
+        tags_container.removeChild(add_tag_popup);
+      });
+
+      let element_tags = element.tags();
+      const available_tags = this._tags.filter(n => !element_tags.includes(n));
+      const tag_list = this._client.ui().document().createElement('ul');
+      tag_list.classList.add('flex-1', 'list-inside', 'list-disc');
+
+      for (const tag of available_tags) {
+        const li = document.createElement('li');
+        li.innerText = tag;
+        li.classList.add('hover:underline', 'cursor-pointer');
+        li.addEventListener('click', () => {
+          log(`Adding tag "${tag}" to element ${element.id()}`);
+          element.add_tag(tag);
+          add_tag_to_row(tags_container, [tag]);
+          tags_container.removeChild(add_tag_popup);
+          this._set_row_editable(row, true);
+        });
+        tag_list.appendChild(li);
+      }
+
+      add_tag_popup.appendChild(tag_list);
+      add_tag_popup.appendChild(close_tag_popup_button);
+
+      tags_container.appendChild(add_tag_popup);
+    })
+    tags_container.appendChild(add_tag_button);
+
+    add_tag_to_row(tags_container, element.tags());
 
     save_button.innerText = 'Save';
     save_button.addEventListener('click', (event) => this._handle_row_save_button_clicked(event.srcElement));
     delete_button.addEventListener('click', (event) => this._handle_row_delete_button_clicked(event.srcElement));
-
     delete_button.innerText = 'Delete';
 
     row.appendChild(text);
     row.appendChild(save_button);
     row.appendChild(delete_button);
+    row.appendChild(tags_container)
 
     row.dataset.row_id = element.id();      
     if (element.is_initialized()) {
@@ -505,7 +664,6 @@ class ListFile extends Base {
 
     let save_button = row.querySelector(`#${ELEMENT_BUTTON_SAVE_ID}`);
     save_button.classList.remove('hidden');
-    this._disable_sorting();
   }
 
   _handle_row_delete_button_clicked(element) {
